bb68658bb6f9c84b0bb13610905ae89d59456cc0
ratishavadze <ratishavadze0@gmail.com>
2024-06-05 18:04:54 +0400

    commit

---
+++ level6/homework.py
@@ -0,0 +1,7 @@
+input("ener youre name : ")
+input("ener youre surname : ")
+int(input("ener youre age : "))
+input("enter youre gmail ")
+int(input("ener youre addresr : "))
+
+
---
+++ level6/homework2.py
@@ -0,0 +1,5 @@
+input("please write youre father's name : " )
+input("please write youre mother's name ': " )
+input("please write youre sister_brather's name: " )
+input("please write youre grandfather's name: " )
+input("please write youre grandmother's name: " )
