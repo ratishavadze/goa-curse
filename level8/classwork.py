140dd355019f4db35c7f82cdf07e3bf1a3169857
ratishavadze <ratishavadze0@gmail.com>
2024-06-10 15:06:28 +0400

    commit

---
+++ level8/classwork.py
@@ -0,0 +1,11 @@
+age = int(input("please enter youre name here : "))
+print(age >= 18 and age <= 25)
+
+
+
+#second classwork
+bugget = int(input("please enter youre bugget"))
+print(bugget > 50 or bugget > 100)
+
+for i in range(5):
+    print("rati shavadze")
