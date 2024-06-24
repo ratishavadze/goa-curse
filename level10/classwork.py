fbe590d0f08f666a0e87bd7fb9971afcd54bd0d8
ratishavadze <ratishavadze0@gmail.com>
2024-06-24 19:14:53 +0400

    commit

---
+++ level10/classwork.py
@@ -0,0 +1,6 @@
+sum = 1
+for i in range(10):
+    num = int(input("please enter number here: "))
+    sum = num * sum
+    
+print("sum")
---
+++ level10/homework.py
@@ -0,0 +1,9 @@
+num1 = int(input("please enter first number here: "))
+num2 = int(input("please enter second number here: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+for i in range(10):
+    print(num1)
+    print(num2)
